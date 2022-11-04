d0d9ebcebd993810e9a203fb14e41463f73311a6
Maxence <maxence.parent@epitech.eu>
2022-11-04 11:38:24 +0100

    [FEAT] init message bar

---
+++ lib/components/fl_message_bar.dart
@@ -0,0 +1,19 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class FLMessageBar extends StatefulWidget {
+  const FLMessageBar({super.key});
+
+  @override
+  State<FLMessageBar> createState() => _FLMessageBar();
+}
+
+class _FLMessageBar extends State<FLMessageBar> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      
+    );
+  }
+}
