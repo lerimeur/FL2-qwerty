298acbeb4db2d6c909222de3bba9d2df9546f185
Maxence <maxence.parent@epitech.eu>
2022-11-04 10:05:15 +0100

    [FEAT] add boolean me

--- fl2_qwerty/lib/components/fl_chat_bubble.dart
+++ fl2_qwerty/lib/components/fl_chat_bubble.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class FLChatBubble extends StatefulWidget {
-  const FLChatBubble({super.key, required this.message});
+  const FLChatBubble({super.key, required this.message, this.me = false});
   final String message;
+  final bool me;
 
   @override
   State<FLChatBubble> createState() => _FLChatBubble();
@@ -13,13 +14,13 @@ class _FLChatBubble extends State<FLChatBubble> {
   Widget build(BuildContext context) {
     return Container(
       width: 150,
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        color: Color.fromRGBO(200, 200, 200, 100),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        color: widget.me ? const Color.fromARGB(255, 0, 130, 255) : const Color.fromRGBO(200, 200, 200, 100),
       ),
       child: Text(
         widget.message,
-        style: const TextStyle(fontSize: 12, color: Colors.black),
+        style: const TextStyle(fontSize: 12, color: Colors.white),
       ),
     );
   }
