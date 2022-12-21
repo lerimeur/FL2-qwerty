98d8dfb7253b9856466e4d7439cdb4c28485e358
Axoloot <cyril.de-lajudie@epitech.eu>
2022-12-21 23:08:30 +0100

    fix: add missing files

--- lib/Component/tab_bar.dart
+++ lib/Component/tab_bar.dart
@@ -1,6 +1,7 @@
 import 'package:fl2_qwerty_messenger/Page/home.dart';
 import 'package:fl2_qwerty_messenger/Page/profle.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyBottomBar extends StatefulWidget {
   const MyBottomBar({super.key});
@@ -24,7 +25,66 @@ class MyBottomBarState extends State<MyBottomBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('fl2_qwerty'),
+        title: Row(
+          children: const <Widget>[
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://i.kym-cdn.com/entries/icons/original/000/028/312/will_poulter.PNG'),
+            ),
+            SizedBox(width: 20),
+            Text(
+              'Chats',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+            )
+          ],
+        ),
+        actions: <Widget>[
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.grey[300],
+            child: IconButton(
+              icon: const Icon(Icons.camera_alt_rounded, color: Colors.black),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+            ),
+          ),
+          const SizedBox(width: 10),
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.grey[300],
+            child: IconButton(
+              icon: const Icon(Icons.person_add_sharp, color: Colors.black),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+            ),
+          ),
+          const SizedBox(width: 10),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(48.0),
+          child: Theme(
+            data: Theme.of(context).copyWith(accentColor: Colors.white),
+            child: Container(
+              height: 45.0,
+              alignment: Alignment.center,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[300],
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: 'Enter a search term',
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
---
+++ lib/Component/top_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatefulWidget {
+  const TopBar({super.key});
+
+  @override
+  State<TopBar> createState() => _TopBarState();
+}
+
+class _TopBarState extends State<TopBar> {
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'people',
+        style: optionStyle,
+      ),
+    );
+  }
+}
